e8a8d19bc10edcee0a894a17809250c47f74bba7
NightmanCZ90 <novak.jan90@gmail.com>
2022-09-05 23:56:49 +0200

    feat: Add sign in logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return user;
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('email in use');
+        }
+      }
+
+      throw err;
+    }
+  }
+
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }
